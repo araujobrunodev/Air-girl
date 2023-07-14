88a463adf826df701e8db2cae2c6684074ef458b
unknown <ballogggamer@gmail.com>
2023-07-14 01:57:44 -0300

    add mainPage children

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
+import { Link } from "react-router-dom"
+import "../css/mainPage.css"
+
 const MainPage = () => {
     return (<>
-    
+        <div id="line-left"></div>
+        <div id="line-right"></div>
+
+        <div className="Container">
+            <div className="Item"></div>
+            <div className="Item"></div>
+            <div className="Item"></div>
+            <div className="Item"></div>
+            <div className="Item"></div>
+            <div className="Item"></div>
+        </div>
+
+        <h1 id="title">Air <br/> girl</h1>
+
+        <Link to={"/playing"}>
+            <button id="mainButton">Play</button>
+        </Link>
     </>)
 }
 
