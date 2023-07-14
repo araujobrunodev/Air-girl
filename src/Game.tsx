eb7b3b0f976d459f410d0467e680f91794de12f3
unknown <ballogggamer@gmail.com>
2023-07-13 23:19:16 -0300

    add DeviceSize into component Game

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import { RouterProvider } from "react-router-dom"
 import router from './router'
+import { DeviceSize } from './globalContext/deviceSize'
 
 function Game() {
+  let [deviceSize,setDeviceSize] = useState(
+    {
+      width: window.screen.width,
+      height: window.screen.height
+    }
+  );
+
   return (<>
+  <DeviceSize.Provider value={{size:deviceSize,setSize:setDeviceSize}}>
     <RouterProvider router={router}/>
+  </DeviceSize.Provider>
   </>)
 }
 
