d629885b9d137a41385d0815036e87bbe69fe5d9
unknown <ballogggamer@gmail.com>
2023-07-20 20:34:21 -0300

    change the way to save components

--- src/elements/background/theme.tsx
+++ src/elements/background/theme.tsx
@@ -4,28 +4,37 @@ import CreateTheme from "./createTheme"
 import createIdentification from "../identification"
 
 const Theme = () => {
-    let childsTheme = useChildsTheme()
+	let childsTheme = useChildsTheme()
 
-    useEffect(() => {
-        let time = setInterval(() => {
-            childsTheme.setChilds([
-                ...childsTheme.childs,
-                <CreateTheme key={createIdentification()}/>
-            ])
-        }, 1000 * 10)
+	useEffect(() => {
+		let time = setInterval(() => {
+			if (childsTheme.childs.length != 1) {
+				childsTheme.setChilds([
+					...childsTheme.childs,
+					{
+						key: createIdentification(),
+						value: 0
+					}
+				])
+			}
+		}, 1000 * 1)
 
-        return () => clearInterval(time);
-    }, [childsTheme.childs])
+		return () => clearInterval(time);
+	}, [childsTheme.childs])
 
-    console.log(childsTheme.childs)
+	console.log(childsTheme.childs)
 
-    return (<>
-        {
-            childsTheme.childs.map((element) => {
-                return element
-            })
-        }
-    </>)
+	return (<>
+		{
+			childsTheme.childs.length != 0 ?
+				childsTheme.childs.map((item) => {
+					return <CreateTheme
+						key={item.key}
+						marginRight={item.value}
+					/>
+				}) : undefined
+		}
+	</>)
 }
 
 export default Theme
