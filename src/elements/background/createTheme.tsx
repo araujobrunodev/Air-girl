8f4ff9908ec174017efbe573bb4fe8f77de976e5
unknown <ballogggamer@gmail.com>
2023-07-20 20:28:13 -0300

    add CreateTheme prop

--- src/elements/background/createTheme.tsx
+++ src/elements/background/createTheme.tsx
@@ -1,16 +1,28 @@
-import { createElement } from "react"
-import createIdentification from "../identification"
+import { FC } from "react"
+import { useDeviceSize } from "../../globalContext/deviceSize"
 
-const CreateTheme = () => {
-    const element = createElement(
-        "img",
-        {
-            src:"../../../public/theme.png",
-            key:createIdentification()
-        }
-    )
+interface CreateThemeProp {
+	marginRight: number
+}
 
-    return element
+const CreateTheme: FC<CreateThemeProp> = ({ marginRight }) => {
+	let deviceSize = useDeviceSize().size
+	let top = deviceSize.height >= 900 ? -2 :
+		deviceSize.height >= 800 ? -9 :
+			deviceSize.height >= 700 ? -24 : 0
+
+	return <>
+		<img
+			src="../../../public/theme.png"
+			style={{
+				display: "absolute",
+				width: deviceSize.width,
+				height: "20rem",
+				marginTop: 90 + top + "%",
+				marginLeft: - marginRight + "px"
+			}}
+		/>
+	</>
 }
 
 export default CreateTheme
