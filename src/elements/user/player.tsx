bc636f5d3a982fe8d5d22df8aaa83f9b7d0a1d2e
unknown <ballogggamer@gmail.com>
2023-07-28 00:21:17 -0300

    add run player

--- src/elements/user/player.tsx
+++ src/elements/user/player.tsx
@@ -3,20 +3,32 @@ import RenderPlayer from "./render"
 
 const Player = () => {
     let [RP,setRP] = useState(<></>)
+    let [spriteNumber,setSpriteNumber] = useState<number>(0)
 
     useEffect(() => {
+        let runP = () => {
+            let fase = setTimeout(() => {
+                if (spriteNumber == 0) return setSpriteNumber(1)
+                if (spriteNumber == 1) return setSpriteNumber(2)
+                if (spriteNumber == 2) return setSpriteNumber(1)
+            },150) 
+
+            return () => clearTimeout(fase)
+        }
+
         let renderP = () => {
             setRP(
                 <RenderPlayer
-                sprite={0}
+                sprite={spriteNumber}
                 posY={-140}
                 />
             )
         }
 
         let Main = setTimeout(() => {
+            runP()
             renderP()
-        },800)
+        },10)
 
         return () => clearTimeout(Main)
     },[RP])
