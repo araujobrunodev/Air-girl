88d09e5831bd8660868b45f595a60fb96ba07901
unknown <ballogggamer@gmail.com>
2023-07-28 00:02:00 -0300

    add render player

--- src/elements/user/player.tsx
+++ src/elements/user/player.tsx
@@ -1,6 +1,28 @@
+import {useState,useEffect} from "react"
+import RenderPlayer from "./render"
+
 const Player = () => {
-    return (<>
-    
+    let [RP,setRP] = useState(<></>)
+
+    useEffect(() => {
+        let renderP = () => {
+            setRP(
+                <RenderPlayer
+                sprite={0}
+                posY={-140}
+                />
+            )
+        }
+
+        let Main = setTimeout(() => {
+            renderP()
+        },800)
+
+        return () => clearTimeout(Main)
+    },[RP])
+
+    return (<>  
+        {RP}  
     </>)
 }
 
