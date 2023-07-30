4c57cb9d8958c1c4a671f592d85c7a24aa04261a
unknown <ballogggamer@gmail.com>
2023-07-29 22:12:09 -0300

    add player.tsx

--- src/elements/user/player.tsx
+++ src/elements/user/player.tsx
@@ -1,17 +1,64 @@
-import {useState,useEffect} from "react"
+import { useState, useEffect } from "react"
 import RenderPlayer from "./render"
+import { usePress } from "../../globalContext/press"
+
 
 const Player = () => {
-    let [RP,setRP] = useState(<></>)
-    let [spriteNumber,setSpriteNumber] = useState<number>(0)
+    let [RP, setRP] = useState(<></>)
+    let [spriteNumber, setSpriteNumber] = useState<number>(0)
+    const defaultPostion = 20
+    let [position,setPosition] = useState(defaultPostion)
+    let [active,setActive] = useState<string>("")
+    let [repeat,setRepeat] = useState(0)
+    let press = usePress()
 
     useEffect(() => {
+        let jumpP = () => {
+            if (!press.event) return;
+
+            let fase1 = setTimeout(() => {
+                let defaultS:number = 45
+
+                if (active == "") { 
+                    setSpriteNumber(5)
+                    setRepeat(repeat+= 1)
+                    setPosition(defaultPostion + (defaultS * repeat))
+                    
+                    if (repeat == 2) setActive(active = "1")
+                    
+                    return;
+                }
+                if (active == "1") {
+                    setSpriteNumber(6)
+                    setRepeat(repeat--)
+                    setPosition(defaultPostion + (defaultS/repeat))
+
+                    if (repeat == 1) setActive("2")
+
+                    return;
+                }
+                
+                if (active == "2") {
+                    setRepeat(0)
+                    setActive("")
+                    setSpriteNumber(1)
+                    setPosition(defaultPostion)
+                    press.setEvent(false)
+                    return;
+                }
+            },70)
+
+            return () => clearTimeout(fase1)
+        }
+
         let runP = () => {
+            if (press.event) return;
+
             let fase = setTimeout(() => {
-                if (spriteNumber == 0) return setSpriteNumber(1)
-                if (spriteNumber == 1) return setSpriteNumber(2)
-                if (spriteNumber == 2) return setSpriteNumber(1)
-            },150) 
+                if (spriteNumber === 0) return setSpriteNumber(1)
+                if (spriteNumber === 1) return setSpriteNumber(2)
+                if (spriteNumber === 2) return setSpriteNumber(1)
+            }, 150)
 
             return () => clearTimeout(fase)
         }
@@ -19,22 +66,23 @@ const Player = () => {
         let renderP = () => {
             setRP(
                 <RenderPlayer
-                sprite={spriteNumber}
-                posY={-140}
+                    sprite={spriteNumber}
+                    posY={-position}
                 />
             )
         }
 
         let Main = setTimeout(() => {
             runP()
+            jumpP()
             renderP()
-        },10)
+        }, 40)
 
         return () => clearTimeout(Main)
-    },[RP])
+    }, [RP])
 
-    return (<>  
-        {RP}  
+    return (<>
+        {RP}
     </>)
 }
 
