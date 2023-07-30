aff8b3a5a6cfb229ccbaee6d2026b2f9c53e0c57
unknown <ballogggamer@gmail.com>
2023-07-30 19:52:21 -0300

    implement CreateLoading to Game

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,9 +1,10 @@
 import router from './router'
 import { useState } from 'react'
-import { CreatePress } from "./globalContext/press"
 import { RouterProvider } from "react-router-dom"
+import { CreatePress } from "./globalContext/press"
 import { DeviceSize } from './globalContext/deviceSize'
 import { ChildsTheme, Item } from './globalContext/childsTheme'
+import { CreateLoading, waitingForTheElement } from './globalContext/loading'
 
 function Game() {
   let [deviceSize, setDeviceSize] = useState(
@@ -14,12 +15,15 @@ function Game() {
   );
   let [childsTheme, setChildsTheme] = useState([] as Item[])
   let [Press, setPress] = useState<boolean>(false)
+  let [waiting, setWaiting] = useState({} as waitingForTheElement)
 
   return (<>
     <DeviceSize.Provider value={{ size: deviceSize, setSize: setDeviceSize }}>
       <ChildsTheme.Provider value={{ childs: childsTheme, setChilds: setChildsTheme }}>
         <CreatePress.Provider value={{ event: Press, setEvent: setPress }}>
-          <RouterProvider router={router} />
+          <CreateLoading.Provider value={{ waiting: waiting, setWaiting: setWaiting }}>
+            <RouterProvider router={router} />
+          </CreateLoading.Provider>
         </CreatePress.Provider>
       </ChildsTheme.Provider>
     </DeviceSize.Provider>
