ac33bb159ecd0d7467239f32d837279bd36323bd
unknown <ballogggamer@gmail.com>
2023-07-30 20:41:37 -0300

    implement loading condition to Play

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,17 +1,25 @@
 import Theme from "../elements/background/theme"
 import Player from "../elements/user/player"
 import TouchScreen from "../elements/touch"
+import { useStart } from "../globalContext/start"
+import Wait from "../elements/load/wait"
 import "../css/play.css"
 
 const Play = () => {
-    
+    let start = useStart
+
     return (<>
-        <Theme/>
+        {!start.canStart ?
+            <Wait /> :
+            <>
+                <Theme />
 
-        <div id="floor"></div>
+                <div id="floor"></div>
 
-        <Player/>
-        <TouchScreen/>
+                <Player />
+                <TouchScreen />
+            </>
+        }
     </>)
 }
 
