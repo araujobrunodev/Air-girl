01f227400a61047f14e33ee46f6f60d184f6ab11
unknown <ballogggamer@gmail.com>
2023-07-30 20:23:04 -0300

    implement CreateStart to Game

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,5 +1,6 @@
 import router from './router'
 import { useState } from 'react'
+import { CreateStart } from "./globalContext/start"
 import { RouterProvider } from "react-router-dom"
 import { CreatePress } from "./globalContext/press"
 import { DeviceSize } from './globalContext/deviceSize'
@@ -16,13 +17,16 @@ function Game() {
   let [childsTheme, setChildsTheme] = useState([] as Item[])
   let [Press, setPress] = useState<boolean>(false)
   let [waiting, setWaiting] = useState({} as waitingForTheElement)
+  let [start, setStart] = useState<boolean>(false)
 
   return (<>
     <DeviceSize.Provider value={{ size: deviceSize, setSize: setDeviceSize }}>
       <ChildsTheme.Provider value={{ childs: childsTheme, setChilds: setChildsTheme }}>
         <CreatePress.Provider value={{ event: Press, setEvent: setPress }}>
           <CreateLoading.Provider value={{ waiting: waiting, setWaiting: setWaiting }}>
-            <RouterProvider router={router} />
+            <CreateStart.Provider value={{ canStart: start, setCanStart: setStart }}>
+              <RouterProvider router={router} />
+            </CreateStart.Provider>
           </CreateLoading.Provider>
         </CreatePress.Provider>
       </ChildsTheme.Provider>
