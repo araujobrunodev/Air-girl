4bb0f5a4da1cfc1d9781af9186a25586943a1132
unknown <ballogggamer@gmail.com>
2023-07-31 00:52:43 -0300

    add callElements.ts

---
+++ src/elements/load/callElements.ts
@@ -0,0 +1,11 @@
+import Floor from "../background/floor"
+import Player from "../user/player"
+import Theme from "../background/theme"
+
+const callElements = () => {
+    Theme()
+    Player()
+    Floor()
+}
+
+export default callElements
