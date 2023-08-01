a8d30423858e1db73bfa247c2aa03cb1389da23c
unknown <ballogggamer@gmail.com>
2023-08-01 20:55:33 -0300

    add topAdd.ts

---
+++ src/elements/topAdd.ts
@@ -0,0 +1,27 @@
+type suggest = "player" | "theme" | "obstacle"
+
+interface TopAddition {
+    to: suggest,
+    size: number
+}
+
+const topAddition = ({ to, size }: TopAddition): number => {
+    let result: number = 0
+
+    switch (to) {
+        case "player":
+            if (size >= 900) result = 20;
+            else if (size >= 880) result = 44;
+						else if (size >= 850) result = 50;
+            else if (size >= 800) result = 70;
+            else if (size >= 700) result = 99;
+            else result = 0
+            break;
+            
+    }
+
+    return result
+}
+
+
+export default topAddition
