585437041f22becc61ba816007a742cb067434e8
unknown <ballogggamer@gmail.com>
2023-08-01 21:11:03 -0300

    change parameter topAddition in Player component

--- src/elements/user/player.tsx
+++ src/elements/user/player.tsx
@@ -1,13 +1,16 @@
+import { useDeviceSize } from "../../globalContext/deviceSize"
 import { useLoading } from "../../globalContext/loading"
 import { usePress } from "../../globalContext/press"
 import { useState, useEffect } from "react"
 import RenderPlayer from "./render"
+import topAddition from "../topAdd"
 
 
 const Player = () => {
 	let press = usePress()
 	let loading = useLoading()
-	const defaultPostion = 20
+	let sizeDevice = useDeviceSize()
+	const defaultPostion = topAddition({to:"player",size:sizeDevice.size.height})
 	let [RP, setRP] = useState(<></>)
 	let [repeat, setRepeat] = useState(0)
 	let [active, setActive] = useState<string>("")
@@ -16,62 +19,62 @@ const Player = () => {
 
 	useEffect(() => {
 		if (typeof press.event === "boolean" &&
-				typeof defaultPostion === "number" &&
-				typeof repeat === "number" &&
-				typeof active === "string" &&
-				typeof position === "number" &&
-				typeof spriteNumber === "number" && 
-				!loading.waiting.player
-				) {
-					loading.setWaiting({
-						bestScore: loading.waiting.bestScore,
-						floor: loading.waiting.floor,
-						gameOver: loading.waiting.gameOver,
-						obstcle: loading.waiting.obstcle,
-						pause: loading.waiting.pause,
-						player: true,
-						score: loading.waiting.score,
-						theme: loading.waiting.theme
-					})
+			typeof defaultPostion === "number" &&
+			typeof repeat === "number" &&
+			typeof active === "string" &&
+			typeof position === "number" &&
+			typeof spriteNumber === "number" &&
+			!loading.waiting.player
+		) {
+			loading.setWaiting({
+				bestScore: loading.waiting.bestScore,
+				floor: loading.waiting.floor,
+				gameOver: loading.waiting.gameOver,
+				obstcle: loading.waiting.obstcle,
+				pause: loading.waiting.pause,
+				player: true,
+				score: loading.waiting.score,
+				theme: loading.waiting.theme
+			})
+		}
+
+		let jumpP = () => {
+			if (!press.event) return;
+
+			let fase1 = setTimeout(() => {
+				let defaultS: number = 45
+
+				if (active == "") {
+					setSpriteNumber(5)
+					setRepeat(repeat += 1)
+					setPosition(defaultPostion + (defaultS * repeat))
+
+					if (repeat == 2) setActive(active = "1")
+
+					return;
+				}
+				if (active == "1") {
+					setSpriteNumber(6)
+					setRepeat(repeat--)
+					setPosition(defaultPostion + (defaultS / repeat))
+
+					if (repeat == 1) setActive("2")
+
+					return;
 				}
 
-			let jumpP = () => {
-				if (!press.event) return;
+				if (active == "2") {
+					setRepeat(0)
+					setActive("")
+					setSpriteNumber(1)
+					setPosition(defaultPostion)
+					press.setEvent(false)
+					return;
+				}
+			}, 70)
 
-				let fase1 = setTimeout(() => {
-					let defaultS: number = 45
-
-					if (active == "") {
-						setSpriteNumber(5)
-						setRepeat(repeat += 1)
-						setPosition(defaultPostion + (defaultS * repeat))
-
-						if (repeat == 2) setActive(active = "1")
-
-						return;
-					}
-					if (active == "1") {
-						setSpriteNumber(6)
-						setRepeat(repeat--)
-						setPosition(defaultPostion + (defaultS / repeat))
-
-						if (repeat == 1) setActive("2")
-
-						return;
-					}
-
-					if (active == "2") {
-						setRepeat(0)
-						setActive("")
-						setSpriteNumber(1)
-						setPosition(defaultPostion)
-						press.setEvent(false)
-						return;
-					}
-				}, 70)
-
-				return () => clearTimeout(fase1)
-			}
+			return () => clearTimeout(fase1)
+		}
 
 		let runP = () => {
 			if (press.event) return;
