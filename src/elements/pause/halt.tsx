0d5e99bef5167327a6f3bbf3407f7b7b150832aa
unknown <ballogggamer@gmail.com>
2023-08-01 22:35:20 -0300

    add halt.tsx

---
+++ src/elements/pause/halt.tsx
@@ -0,0 +1,23 @@
+import { usePause } from "../../globalContext/pause"
+
+const Halt = () => {
+	let pause = usePause()
+
+	return (<>
+		{!pause.active ?
+			<button
+				onClick={() => { return pause.setActive(true) }}
+				id="buttonResume"
+			>
+				<img src="../../../unpause.svg" alt="unpause" />
+			</button>
+			:
+			<p
+				id="pause"
+				onClick={() => {return pause.setActive(false)}}
+			>Pause</p>
+		}
+	</>)
+}
+
+export default Halt
