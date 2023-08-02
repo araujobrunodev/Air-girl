f4dd3d27f27f1455a3fa6a0e7f32d3fbc6687555
unknown <ballogggamer@gmail.com>
2023-08-02 20:30:10 -0300

    implement baseTheme condition and change size player in topAddition

--- src/elements/topAdd.ts
+++ src/elements/topAdd.ts
@@ -1,4 +1,4 @@
-type suggest = "player" | "theme" | "obstacle"
+type suggest = "player" | "baseTheme" | "theme" | "obstacle"
 
 interface TopAddition {
     to: suggest,
@@ -10,12 +10,12 @@ const topAddition = ({ to, size }: TopAddition): number => {
 
     switch (to) {
         case "player":
-            if (size >= 900) result = 20;
-            else if (size >= 880) result = 44;
-						else if (size >= 850) result = 50;
-            else if (size >= 800) result = 70;
-            else if (size >= 700) result = 99;
-            else result = 0
+            if (size >= 900) result = -590;
+            else if (size >= 880) result = -567;
+						else if (size >= 850) result = -544;
+            else if (size >= 800) result = -507;
+            else if (size >= 700) result = -474;
+            else result = -500
             break
         case "theme":
             if (size >= 900) result = 20;
@@ -23,6 +23,16 @@ const topAddition = ({ to, size }: TopAddition): number => {
             else if (size >= 700) result = 1;
             else result = 0
             break       
+        case "baseTheme":
+            if (size >= 920) result = 47;
+            else if (size >= 900) result = 45;
+            else if (size >= 890) result = 53;
+            else if (size >= 880) result = 45;
+            else if (size >= 850) result = 27;
+            else if (size >= 840) result = 20;
+            else if (size >= 800) result = -3;
+            else if (size >= 700) result = 7;
+            break
     }
 
     return result
