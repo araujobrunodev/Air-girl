97d1fb50a6d78e62cee2943456aa7d4670cc0a11
unknown <ballogggamer@gmail.com>
2023-08-02 18:41:56 -0300

    implement Halt to callElements

--- src/elements/load/callElements.ts
+++ src/elements/load/callElements.ts
@@ -2,12 +2,14 @@ import Theme from "../background/theme"
 import Floor from "../background/floor"
 import Player from "../user/player"
 import Score from "../time/score"
+import Halt from "../pause/halt"
 
 const callElements = () => {
     Player()
     Theme()
     Floor()
     Score()
+    Halt()
 }
 
 export default callElements
