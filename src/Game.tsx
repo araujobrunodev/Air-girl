e52ce8a93ac2a0f8a3a169e2f5fe3dd33a3e0da6
unknown <ballogggamer@gmail.com>
2023-08-03 22:11:42 -0300

    implement CreateObstacle to Game component

--- src/Game.tsx
+++ src/Game.tsx
@@ -7,30 +7,32 @@ import { CreateStart } from "./globalContext/start"
 import { CreatePress } from "./globalContext/press"
 import { DeviceSize } from './globalContext/deviceSize'
 import { ChildsTheme, Item } from './globalContext/childsTheme'
+import { CreateObstacle, itemObstacle } from './globalContext/childsObstacle'
 import { CreateLoading, waitingForTheElement } from './globalContext/loading'
 
 function Game() {
+  let [score, setScore] = useState<number>(0)
+  let [Press, setPress] = useState<boolean>(false)
+  let [start, setStart] = useState<boolean>(false)
+  let [pause, setPause] = useState<boolean>(false)
+  let [childsTheme, setChildsTheme] = useState([] as Item[])
+  let [obstacle,setObstacle] = useState([] as itemObstacle[])
   let [deviceSize, setDeviceSize] = useState(
     {
       width: window.screen.width,
       height: window.screen.height
     }
   );
-  let [childsTheme, setChildsTheme] = useState([] as Item[])
-  let [Press, setPress] = useState<boolean>(false)
   let [waiting, setWaiting] = useState<waitingForTheElement>({
     bestScore:false,
-    floor:false,
     gameOver:false,
     obstacle:false,
-    pause:false,
     player:false,
+    floor:false,
+    pause:false,
     score:false,
     theme:false
   })
-  let [start, setStart] = useState<boolean>(false)
-  let [score, setScore] = useState<number>(0)
-  let [pause, setPause] = useState<boolean>(false)
 
   return (<>
     <DeviceSize.Provider value={{ size: deviceSize, setSize: setDeviceSize }}>
@@ -40,7 +42,9 @@ function Game() {
             <CreateStart.Provider value={{ canStart: start, setCanStart: setStart }}>
               <CreateScore.Provider value={{pits:score,setPits:setScore}}>
                 <CreatePause.Provider value={{active:pause,setActive:setPause}}>
-                  <RouterProvider router={router} />
+                  <CreateObstacle.Provider value={{item:obstacle,setItem:setObstacle}}>
+                    <RouterProvider router={router} />
+                  </CreateObstacle.Provider>
                 </CreatePause.Provider>
               </CreateScore.Provider>
             </CreateStart.Provider>
