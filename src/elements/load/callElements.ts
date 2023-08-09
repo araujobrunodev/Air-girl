573d3d89b0b6d674e0e6909e267bb71681b08e78
unknown <ballogggamer@gmail.com>
2023-08-08 21:22:32 -0300

    implement obstacle in callElements

--- src/elements/load/callElements.ts
+++ src/elements/load/callElements.ts
@@ -3,10 +3,12 @@ import Floor from "../background/floor"
 import Player from "../user/player"
 import Score from "../time/score"
 import Halt from "../pause/halt"
+import Obstacle from "../challenge/obstacle"
 
 const callElements = () => {
     Player()
     Theme()
+    Obstacle()
     Floor()
     Halt()
     Score()
