c0768da2f7f65c31817ce0c4923c4e800f460acd
unknown <ballogggamer@gmail.com>
2023-08-12 19:55:51 -0300

    implement signature setActive to GameOver

--- src/globalContext/gameover.ts
+++ src/globalContext/gameover.ts
@@ -1,7 +1,8 @@
 import { useContext,createContext } from "react";
 
 interface GameOver {
-    active:boolean
+    active:boolean,
+    setActive:(ac:boolean) => void
 }
 
 const CreateGameOver = createContext({} as GameOver)
