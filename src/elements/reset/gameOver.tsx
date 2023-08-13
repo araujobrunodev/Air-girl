7cac01a600f36b049377ec271984442cafac772a
unknown <ballogggamer@gmail.com>
2023-08-13 20:28:59 -0300

    implement restart condition to GameOver component

--- src/elements/reset/gameOver.tsx
+++ src/elements/reset/gameOver.tsx
@@ -1,15 +1,25 @@
 import { useObstacle } from "../../globalContext/childsObstacle"
+import { useChildsTheme } from "../../globalContext/childsTheme"
+import { usePermision } from "../../globalContext/permision"
 import { useGameOver } from "../../globalContext/gameover"
 import { useLoading } from "../../globalContext/loading"
 import { useScore } from "../../globalContext/score"
+import { useStart } from "../../globalContext/start"
+import { usePress } from "../../globalContext/press"
 import { useEffect } from "react"
 import "../../css/gameOver.css"
+import { usePause } from "../../globalContext/pause"
 
 const GameOver = () => {
 	let score = useScore()
+	let start = useStart()
+	let press = usePress()
+	let pause = usePause()
 	let loading = useLoading()
 	let obstacle = useObstacle()
 	let gameover = useGameOver()
+	let permision = usePermision()
+	let childsTheme = useChildsTheme()
 
 	useEffect(() => {
 		if (!loading.waiting.gameOver &&
@@ -26,26 +36,35 @@ const GameOver = () => {
 				theme: loading.waiting.theme
 			})
 		}
-	}, [loading.waiting])
+
+		if (gameover.active) {
+			press.setEvent(false)
+			pause.setActive(true)
+		}
+	}, [loading.waiting,gameover.active,press.event,pause.active])
 
 	return (<>
 		<button
 			id="button-gameover"
 			onClick={() => {
-				score.setPits(0)
-
 				loading.setWaiting({
 					bestScore: false,
-					floor: false,
 					gameOver: false,
 					obstacle: false,
-					pause: false,
 					player: false,
+					floor: false,
+					pause: false,
 					score: false,
-					theme: false
+					theme: loading.waiting.theme
 				})
-
+				
+				score.setPits(0)
 				obstacle.setItem([])
+				childsTheme.setChilds([])
+				permision.setCreate(false)
+				gameover.setActive(false)
+				pause.setActive(false)
+				start.setCanStart(false)
 			}}>
 			Try again
 		</button>
