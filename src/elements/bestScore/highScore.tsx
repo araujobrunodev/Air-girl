ed227a7f7d42eb5cb6c68c672f99055a410125ce
unknown <ballogggamer@gmail.com>
2023-08-15 19:50:49 -0300

    implement loading condition to BestScore component

--- src/elements/bestScore/highScore.tsx
+++ src/elements/bestScore/highScore.tsx
@@ -1,12 +1,28 @@
+import { useLoading } from "../../globalContext/loading"
 import { useRecord } from "../../globalContext/record"
 import { useEffect } from "react"
 import "../../css/bestScore.css"
 
 const BestScore = () => {
 	const record = useRecord()
+	const loading = useLoading()
 
 	useEffect(() => {
 		let LS = localStorage.getItem("bestScore")
+
+		if (typeof record.value != "undefined" &&
+			!loading.waiting.bestScore) {
+			loading.setWaiting({
+				obstacle: loading.waiting.obstacle,
+				gameOver: loading.waiting.gameOver,
+				bestScore: loading.waiting.bestScore = true,
+				player: loading.waiting.player,
+				floor: loading.waiting.floor,
+				pause: loading.waiting.pause,
+				score: loading.waiting.score,
+				theme: loading.waiting.theme
+			})
+		}
 
 		let setNewValue = setInterval(() => {
 			if (LS != undefined)
