21c13d68fe1fa9b460bd4ea34ba8b7bf36c687ef
unknown <ballogggamer@gmail.com>
2023-08-16 19:46:58 -0300

    implement size reduction in obstacleSize

--- src/elements/challenge/obstacleSize .ts
+++ src/elements/challenge/obstacleSize .ts
@@ -9,21 +9,22 @@ interface DefaultSize {
 
 const obstacleSize = ({name}:DefaultObstacleSize) => {
     let result:DefaultSize = {height:0,width:0} 
+    let less = 15
 
     switch (name) {
         case "car":
-            result.width = 120
-            result.height = 55
+            result.width = 120 - less
+            result.height = 55 - less
             break
 
         case "rock":
-            result.height = 75
-            result.height = 60
+            result.height = 75 - less
+            result.height = 60 - less
             break
 
         case "traffic cone":
-            result.width = 50
-            result.height = 53
+            result.width = 50 - less
+            result.height = 53 - less
             break
     }
 
