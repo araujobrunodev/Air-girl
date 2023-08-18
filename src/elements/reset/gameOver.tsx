826d94a7bdbbc83c2a2cae6d39a74b12c9d1456a
unknown <ballogggamer@gmail.com>
2023-08-18 18:44:38 -0300

    implement button effect in GameOver component

--- src/elements/reset/gameOver.tsx
+++ src/elements/reset/gameOver.tsx
@@ -7,7 +7,7 @@ import { useScore } from "../../globalContext/score"
 import { useStart } from "../../globalContext/start"
 import { usePress } from "../../globalContext/press"
 import { usePause } from "../../globalContext/pause"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import "../../css/gameOver.css"
 
 const GameOver = () => {
@@ -16,10 +16,14 @@ const GameOver = () => {
 	let press = usePress()
 	let pause = usePause()
 	let loading = useLoading()
+	const defaultBorderSize = 3
+	const defaultBackgroundColor = "#FF7A00"
 	let obstacle = useObstacle()
 	let gameover = useGameOver()
 	let permision = usePermision()
 	let childsTheme = useChildsTheme()
+	let [backgroundColor, setBackgroundColor] = useState(defaultBackgroundColor)
+	let [borderSize, setBorderSize] = useState(defaultBorderSize)
 
 	useEffect(() => {
 		if (!loading.waiting.gameOver &&
@@ -46,33 +50,48 @@ const GameOver = () => {
 	return (<>
 		<button
 			id="button-gameover"
+			className="gameover"
 			onClick={() => {
 				let LS = localStorage.getItem("bestScore")
+				setBackgroundColor("purple")
+				setBorderSize(7)
 
-				loading.setWaiting({
-					bestScore: false,
-					gameOver: false,
-					obstacle: false,
-					player: false,
-					floor: false,
-					pause: false,
-					score: false,
-					theme: loading.waiting.theme
-				})
+				setTimeout(() => {
+					loading.setWaiting({
+						bestScore: false,
+						gameOver: false,
+						obstacle: false,
+						player: false,
+						floor: false,
+						pause: false,
+						score: false,
+						theme: loading.waiting.theme
+					})
 
-				if (LS != undefined) {
-					if (JSON.parse(LS) < score.pits)
+					permision.setCreate(false)
+					pause.setActive(false)
+					score.setPits(0)
+
+					if (LS != undefined) {
+						if (JSON.parse(LS) < score.pits)
+							localStorage.setItem("bestScore", JSON.stringify(score.pits))
+					} else
 						localStorage.setItem("bestScore", JSON.stringify(score.pits))
-				} else
-					localStorage.setItem("bestScore", JSON.stringify(score.pits))
 
-				score.setPits(0)
-				obstacle.setItem([])
-				childsTheme.setChilds([])
-				permision.setCreate(false)
-				gameover.setActive(false)
-				pause.setActive(false)
-				start.setCanStart(false)
+					setBackgroundColor(defaultBackgroundColor)
+					setBorderSize(defaultBorderSize)
+
+					obstacle.setItem([])
+					childsTheme.setChilds([])
+					gameover.setActive(false)
+					start.setCanStart(false)
+				}, 1000)
+			}}
+			style={{
+				border: borderSize + "px solid black",
+				boxShadow: "2px 4px 3px black",
+				transition: "all 100ms",
+				background: backgroundColor
 			}}>
 			Try again
 		</button>
