328a7545ca8595a88e1ce25d9e762d9321f2d330
unknown <ballogggamer@gmail.com>
2023-08-18 21:22:48 -0300

    add page404.tsx

---
+++ src/pages/page404.tsx
@@ -0,0 +1,23 @@
+const PageNotFound = () => {
+    return (<>
+        <h1>Page not found</h1>
+
+        <ul>
+            <li>
+                try more later
+            </li>
+            
+            <li>
+                reload the page
+            </li>
+            
+            <li>
+                report the problem <a href="" target="_blank" id="link">here</a>
+            </li>
+        </ul>
+
+        <h2>status 404</h2>
+    </>)
+}
+
+export default PageNotFound
