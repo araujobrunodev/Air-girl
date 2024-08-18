2f077d4efc3eb8baedca78387ce691a1401f018c
unknown <ballogggamer@gmail.com>
2024-08-18 17:40:01 -0300

    add deloy on load before start the game

--- src/elements/load/wait.tsx
+++ src/elements/load/wait.tsx
@@ -1,7 +1,6 @@
 import { useLoading } from "../../globalContext/loading"
 import { useStart } from "../../globalContext/start"
 import { useEffect, useState } from "react"
-import allSprite from "../user/sprite"
 import LoadImages from "./loadImages"
 import "../../css/wait.css"
 
@@ -24,7 +23,6 @@ const Wait = () => {
 	return (
 		<div id="divWait">
 			<LoadImages/>
-			<img src={allSprite[0].Path} id="iconWait"></img>
 
 			<h1 id="message">{ message }</h1>
 		</div>
